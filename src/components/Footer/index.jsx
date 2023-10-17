7a24de7ed950e608a6df6e210d6f58164c8f01e4
Davranbek <davran9559@gmail.com>
2023-10-17 20:08:46 +0500

    start project

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+const index = () => {
+    return (
+        <footer>
+            <h2>footer</h2>      
+        </footer>
+    );
+};
+
+export default index;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+const index = () => {
+    return (
+        <header>
+            <h2>header</h2> 
+        </header>
+    );
+};
+
+export default index;
